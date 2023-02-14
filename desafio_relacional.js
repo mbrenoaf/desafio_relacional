02a06b4464cab7320583af0d7d72ef59defbdf29
Marcos Breno <123556594+mbrenoaf@users.noreply.github.com>
2023-02-14 15:30:03 -0300

    Add files via upload

---
+++ desafio_relacional.js
@@ -0,0 +1,14 @@
+let a = 1
+let b = 2
+
+console.log(a == b)
+console.log(a === b)
+console.log(a >= b)
+console.log(b < a)
+console.log(b == a)
+
+console.log(a < b)
+console.log(a !== b)
+console.log(a != b)
+console.log(b > a)
+console.log(b >= a )
